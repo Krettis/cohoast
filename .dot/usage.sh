a40a9b4e8f2211bb703e7a6aa4a7a04fd27bcad1
Krettis <krettis@gmail.com>
2015-04-29 23:37:25 +0200

    ch-05: usages are now in text-format. Much easier to maintain for the future.

--- .dot/usage.sh
+++ .dot/usage.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
-
-
-
 function usage
 {
-      echo -e "usage: cohoast optional: [[add ] [-h | --help] ]\n"
-			echo "Only calling 'cohoast' will take you to a friendly menu"
-			echo "where you can specify a lot more options for handling"
-			echo -e "your hostfile.\n"
+	cat $DIR.dot/usage/main.txt
+}
 
-			echo "List of commands: "
-      echo "add | a        Add a hostname to the localhost"
- 
-			#echo "-h | --help     Get more information about"
-      #echo "               how to use a command"
-
+function	usage_add
+{
+	cat $DIR.dot/usage/add.txt
+}
      #echo "remove | rm   Invoke the remove-host command"
       #echo "-f --file     The configuration file, in $BASEPATH/sites"
       #echo "ports         Lists the currently used ports per hostname"
       #echo "ls | l        Lists the hosts configuration files"
       #kecho "usage: cohoast [[add | a | remove | rm] [-h | --help] [-f | --file] [ ports ] [ ls | l ]]"
-}
-
-function usage_add
-{
-#	echo "usage: cohoast add [ hostname ]"
-#	echo "This will add a hostname to hosts-file"
-}
