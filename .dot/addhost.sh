b71a4a415ba6ef466e56c20df791c6c4d1d48316
Krettis <krettis@gmail.com>
2015-05-02 23:08:21 +0200

    ch-08: first setup for working with the new category

--- .dot/addhost.sh
+++ .dot/addhost.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-
 addHost(){
-	echo $HOSTS_FILE" "$portnumber"adding host : "$1
-	categoryformat="\n\n\n\n\t#####\n\t## $category\n\t#####\n\n\n" 
+	categoryname=$(echo $category | tr '[:lower:]' '[:upper:]')
+	categoryformat="\n"$(cat $DIR.dot/art/category.txt | sed 's/CATEGORYNAME/'$categoryname'/g' | sed 's/nr/1/g' | sed 's/date_c/'$currentdate'/g' | sed 's/date_u/'$currentdate'/g')"\n\n\n\n\n\n"
 	lineformat=$(echo -e "$ipaddress:$portnumber\t$hostname\r")
+	echo -e "Adding host\nname: "$hostname"\nPortnumber: "$portnumber"\ncategory: "$category"\nIn file:"$HOSTS_FILE
+echo $categoryname" "$lineformat
 
 	sed '/'"$lineformat"'/ d' $HOSTS_FILE > hosts.tmp
-	foundcategory=$(sed -n '/'"## $category"'/ =' hosts.tmp)
+	foundcategory=$(sed -n '/'"#  $categoryname"'/ =' hosts.tmp)
 	lineappend=3
+echo "found category: "$foundcategory
 
 	if [[ $foundcategory == '' ]] ; then
+		currentdate=$(date "+%Y-%m-%d")
+	  lineappend=12
+
 		echo -e "$categoryformat" >> hosts.tmp
-	       lineappend=6
 	fi
 
 	insertline=$(($foundcategory+$lineappend))
